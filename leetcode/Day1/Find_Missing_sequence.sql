3fccb11ca29238f3fe5f188fa8a26c8d6436a905
Ravi <ravi.verma278@gmail.com>
2023-06-07 00:33:23 +0530

    Find missing sequence

---
+++ leetcode/Day1/Find_Missing_sequence.sql
@@ -0,0 +1,103 @@
+MY SQL:
+-----------------
+WITH RECURSIVE numbers AS (
+    SELECT 0 AS DATE
+   UNION ALL
+   SELECT DATE + 1
+   FROM numbers
+   WHERE DATE < 10)
+SELECT * FROM numbers;
+
+POSTGRES:
+----------------------
+WITH RECURSIVE Emp_CTE(ID) AS
+(
+SELECT 1
+UNION ALL
+SELECT ID + 1 FROM Emp_CTE WHERE ID < 14
+)
+SELECT * FROM Emp_CTE
+
+
+
+PART1: 
+
+CREATE TABLE tblEmployee(EmpID INT PRIMARY KEY     NOT NULL);
+
+INSERT INTO tblEmployee(EmpID) VALUES (14);
+
+Select * from tblEmployee;
+1
+4
+5
+7
+10
+12
+14
+
+WITH RECURSIVE Emp_CTE(ID) AS
+(
+SELECT 1 --------> This is called Anchor Query
+UNION ALL
+SELECT ID + 1 FROM Emp_CTE WHERE ID < 14 ----------------> Recursive Query
+)
+--SELECT * FROM Emp_CTE
+
+SELECT EC.ID AS Missing_Sequence FROM Emp_CTE EC 
+LEFT JOIN tblEmployee E
+ON EC.ID = E.EmpID 
+WHERE E.EmpID IS NULL
+
+
+PART2:
+
+CREATE TABLE tblEmployee1 (EmpID INT);
+INSERT into tblEmployee1 values (51000),(51001),(51004),(51009),(51013),(51017),(51020),(51025),(51026);
+
+select * from tblEmployee1;
+
+51000
+51001
+51004
+51009
+51013
+51017
+51020
+51025
+51026
+
+WITH RECURSIVE Emp_CTE(ID) AS
+(
+SELECT 51000
+UNION ALL
+SELECT ID + 1 FROM Emp_CTE WHERE ID < 51026
+)
+--SELECT * FROM Emp_CTE
+
+SELECT EC.ID AS Missing_Sequence FROM Emp_CTE EC 
+LEFT JOIN tblEmployee1 E
+ON EC.ID = E.EmpID 
+WHERE E.EmpID IS NULL
+
+
+Missing_Sequence
+---------------------------
+51002
+51003
+51005
+51006
+51007
+51008
+51010
+51011
+51012
+51014
+51015
+51016
+51018
+51019
+51021
+51022
+51023
+51024
+
--- leetcode/Day1/Uber_Profitride.sql
+++ leetcode/Day1/Uber_Profitride.sql
@@ -1,3 +1,6 @@
+https://www.linkedin.com/posts/ankitbansal6_sql-interview-uber-activity-7051406885186404353-Ibja/
+
+
 Write a query to find total rides and profit rides by each driver. 
 
 Profit ride is when the end location of the current ride is the same as the start location of the next ride.
@@ -42,4 +45,122 @@ id		total_rides	profitable_rides
 dri_1	5			1
 dri_2	2			0
 
+
+
+
+1] Using LAG / LEAD
+
+WITH prev_loc AS (
+SELECT *,
+ LAG(end_loc) OVER(Partition by id order by start_time asc) as prev_end_loc
+ FROM 
+ Drivers
+ )
+  
+SELECT 
+id, 
+COUNT(id) as total_rides,
+SUM(CASE WHEN prev_end_loc = start_loc THEN 1 ELSE 0 END) AS profit_rides
+FROM 
+prev_loc
+  GROUP BY 1
+
+
+2] Without Using LAG / LEAD
+
+WITH profit_rides AS (
+SELECT 
+distinct drivers1.id
+ as profit_rider,
+drivers1.end_time 
+FROM Drivers as drivers1
+ INNER JOIN 
+ Drivers as drivers2
+ ON (drivers1.id
+ = drivers2.id
+ and drivers1.end_loc = drivers2.start_loc)
+ )
+
+ SELECT 
+  id,
+  COUNT(*) as total_rides,
+  COUNT(profit.profit_rider) / 2 as profit_rides
+  FROM drivers drive
+  LEFT JOIN 
+  Profit_rides profit
+  ON (profit.profit_rider = drive.id
+ and profit.end_time = drive.end_time)
+  GROUP BY 1
+  
+  
+----------------------------------------------------------------
+WITH CTE AS (
+SELECT id,COUNT(1) as total_rides from drivers
+group by id),
+CTE2 as (
+SELECT COUNT(1) AS Profit_rides,id FROM (SELECT id,start_loc,end_loc,lag(end_loc) 
+over (order by id) as LAST_LOC
+from drivers)abc
+where start_loc=LAST_LOC
+group by id)
+SELECT CTE.id
+,CTE.total_rides,ISNULL(CTE2.Profit_rides,0) AS Profit_rides FROM CTE left join CTE2 ON 
+CTE.id
+=CTE2.id
+
+
+---------------------------------------------------------
+with cte as (select *, rank() over (partition by id order by end_time) as r from drivers), cte2 as (select a.id
+, count(1) as profit_rides from cte a inner join cte b on a.id
+ = b.id
+ and a.end_loc = b.start_loc and a.r+1 = b.r group by a.id
+), cte3 as (select id, count(*) as total_rides from drivers group by id) (select cte3.id
+, total_rides, if(profit_rides is NULL, 0, profit_rides) as profit_rides from cte3 left join cte2 on cte3.id
+ = cte2.id
+);
+
+
+------------------------------------
+with profit_rides as (
+select d1.id
+ as id, count(*)/2 as profit_rides from 
+dbo.drivers d1 join dbo.drivers d2
+on d1.id
+ = d2.id
+ and d1.start_loc = d2.end_loc
+group by d1.id
+),
+
+total_rides as(
+select id, count(*) as total_rides
+from dbo.drivers
+group by id)
+
+select total_rides.id
+, total_rides, ISNULL(profit_rides,0) as profit_rides
+from total_rides left join profit_rides
+on profit_rides.id
+ = total_rides.id
+
+
+
+-----------------------------------------------------------------------------
+with cte as (
+select *,row_number() over (partition by id order by start_time) as num
+from drivers
+)  
+select 
+distinct c1.id
+,
+count(c1.id
+) over (partition by c1.id
+) as total_rides,
+count(c2.end_loc) over (partition by c1.id
+) as profitable_rides
+from cte as c1
+left join cte as c2
+on c1.id
+ = c2.id
+ and c1.end_loc = c2.start_loc
+and c1.num = c2.num-1
 
