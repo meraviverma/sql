4907fd12f1f4b51d3592757a316b4f23f0aa66de
Ravi <ravi.verma278@gmail.com>
2026-06-17 20:59:02 +0530

    FInd missing number

---
+++ 2026_SqlChallenge/Day6_FIndMissingNumber.sql
@@ -0,0 +1,11 @@
+-- You're given a table containing:
+-- 1, 2, 3, 4, 5, 10
+
+-- 🎯 The task:
+-- Find all the missing numbers between the minimum and maximum values.
+-- Simple?
+
+-- Then comes the twist.
+-- 🚫 Recursive CTEs are NOT allowed.
+-- Here's a SQL query that finds all the missing numbers between the minimum and maximum values in the given table without using recursive CTEs:
+
